1cad149e99d6e9df379d8c30c7349f44219960fe
Mikael <git@evinja.se>
2015-04-19 17:36:06 +0200

    Logo and login design

---
+++ app/src/main/java/se/evinja/hazeldroid/Activity_Login.java
@@ -0,0 +1,24 @@
+package se.evinja.hazeldroid;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+
+public class Activity_Login extends ActionBarActivity {
+//    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+//        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+//        setSupportActionBar(mToolbar);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+
+}
